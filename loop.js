07bed3770e5a85a5ba72c663cf64ba014c5b834d
Hridoy Mnd <hridoymnd10@gmail.com>
2026-01-27 09:59:45 +0600

    added js-loop task 1

---
+++ loop.js
@@ -0,0 +1,5 @@
+// task 1 ( "I will invest at lest 6 hours every day for next 60 days!" display this massage 60 time)
+
+for (let day = 1; day <= 60; day++) {
+    console.log("I will invest at least 6 hours every day for next 60 day");
+}
