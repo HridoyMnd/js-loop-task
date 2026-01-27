79c3b3fc233e4fd55764156a9f0949a8a662ede5
Hridoy Mnd <hridoymnd10@gmail.com>
2026-01-27 11:29:17 +0600

    added multiplicationTable

---
+++ multiplicationTable.js
@@ -0,0 +1,11 @@
+// As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+let multiplicand = 5;
+let multiplier = 1;
+let product = 0;
+
+while (product < 50) {
+    product = multiplicand * multiplier
+    multiplier++;
+    console.log(`${multiplicand} * ${multiplier} = ${product}`);
+
+}
